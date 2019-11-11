426e80dfc7fd9603a2b654d133718da75105fd42
tamdm-dsoft <tamdm@dsoft.com.vn>
2019-11-12 01:55:27 +0700

    remove .gradle and update the gradle to 5.2.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,22 @@
+/**
+ * The type Main.
+ */
 public class Main {
+
+    /**
+     * Just start to see java docwork.
+     *
+     * @param hello the hello
+     */
+    public void justStartToSeeJavaDocwork(String hello) {
+        System.out.println(hello);
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         ((Runnable) () -> System.out.println("Hello World")).run();
     }
